cba892a96b56c07c66cc03230f6a723b961de4bc
Jaerker <johanjakberger@gmail.com>
2023-12-06 19:08:10 +0100

    Fixing alot of stuff

--- src/js/script.js
+++ src/js/script.js
@@ -3,12 +3,12 @@ window.addEventListener("scroll", () => {
     let scrollTop = window.scrollY || document.documentElement.scrollTop;
     let header = this.document.querySelector("#header");
 
-    if (scrollTop > 250) {
-        header.classList.remove("header");
+    if (scrollTop > 150) {
+        header.classList.remove("header--scrolled-up");
         header.classList.add("header--scrolled-down");
     }
     else {
-        header.classList.add("header");
+        header.classList.add("header--scrolled-up");
         header.classList.remove("header--scrolled-down");
 
     }
